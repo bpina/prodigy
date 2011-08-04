e5875021cf88b1f20985ed6370a773a8f8301edb
brian <brian@debian.local>
2011-08-04 18:19:41 -0400

    adding in exception handling for default_character context processor

--- characters/context_processors.py
+++ characters/context_processors.py
@@ -1,6 +1,12 @@
+from django.core.exceptions import ObjectDoesNotExist
+
 def default_character(request):
   if not request.user.is_anonymous() and request.user.is_authenticated:
-    c = request.user.character_set.get(is_default=True)
+    try:
+      c = request.user.character_set.get(is_default=True)
+    except ObjectDoesNotExist:
+      c = None
+
     return {'default_character': c}
   else:
     return {}

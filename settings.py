65603f9b0812055303276d58a6220844e0d6de53
bpina <brian.pina@gmail.com>
2012-04-24 12:20:47 -0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -13,8 +13,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'tundra',                      # Or path to database file if using sqlite3.
-        'USER': 'tundra',                      # Not used with sqlite3.
-        'PASSWORD': '_tundra_MySQ1_',                  # Not used with sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
